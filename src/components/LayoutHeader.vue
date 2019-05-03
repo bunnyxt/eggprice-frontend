f3e4f08ccae71b5f8c18aebc3f697b300a0d6cfc
bunnyxt <bunnyxt@outlook.com>
2019-05-03 22:55:34 +0800

    Update header with logo.

--- src/components/LayoutHeader.vue
+++ src/components/LayoutHeader.vue
@@ -1,17 +1,20 @@
 <template>
-  <div class="header">
+  <div style="padding: 16px">
     <!-- 需要一个标题  右侧一个登陆注册按钮 -->
+    <div style="background: #FFF; padding: 8px; height: 50px; line-height: 0px">
+      <img src="/logo.png" style="width:32px; height:32px;"/>
+      <span style="margin-left: 8px; font-size: 20px; vertical-align: middle">鸡蛋价格监测</span>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: "LayoutHeader"
+  name: "LayoutHeader",
+  data: function() {
+    return {
+
+    }
+  }
 }
 </script>
-
-<style>
-.header {
-  
-}
-</style>
